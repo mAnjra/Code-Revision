8e95fc403068c4ba46ff5b3e5bcb587b28dcee55
mAnjra <39240839+mAnjra@users.noreply.github.com>
2018-11-30 18:14:19 +0000

    Update ex37.java

--- Methods/ex37.java
+++ Methods/ex37.java
@@ -1,8 +1,3 @@
-/***
- * Exercise 37 on MOOC.
- * This is to do with making your own methods, 
- * and calling said mehtods in your main method.
- */
 public class ex37{
     public static void main(String[] args){
         printText();
