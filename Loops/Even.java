e0e3fa7107bacae16b9eb4dd57128e3f92713837
mAnjra <39240839+mAnjra@users.noreply.github.com>
2018-11-30 17:47:57 +0000

    Add files via upload

---
+++ Loops/Even.java
@@ -0,0 +1,18 @@
+public class Even{
+    public static void main(String[] args){
+        
+        for(int i = 0; i<=100;i++){
+            if(i%2 != 0){
+                
+            }else{
+                System.out.println(i);
+            }
+        }
+
+        
+
+
+
+
+    }
+}
