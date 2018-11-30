1c630ea4774c6891206398ab6c88f94f125d417e
mAnjra <39240839+mAnjra@users.noreply.github.com>
2018-11-30 17:54:48 +0000

    Update ZeroHundred.java

--- Loops/ZeroHundred.java
+++ Loops/ZeroHundred.java
@@ -4,7 +4,6 @@ public class ZeroHundred{
 
         // 0 TO 100
         int i;
-
         for(i=0;i<=100;i++){
             System.out.println(i);
         }
@@ -13,14 +12,12 @@ public class ZeroHundred{
         for(i=100; i>=0;i--){ // if i is less than 100 which it is at this moment, then print it, when the condition fails thats when the loop ends.
             System.out.println(i);
         }
+        
         //WHILE LOOP - 0 - 100
         int e = 0;
         while(e<=100){
             System.out.println(e);
             e++;
         }
-
-
     }
-
-    }
+}
