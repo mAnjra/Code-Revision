6bf2c460f45529920471340396d8f2882834bef2
mAnjra <39240839+mAnjra@users.noreply.github.com>
2018-12-03 17:07:41 +0000

    Add files via upload

---
+++ Average/Average.java
@@ -0,0 +1,116 @@
+import java.awt.Desktop;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Average implements Serializable{
+
+    
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main (String args[]){
+
+
+        ArrayList<Integer> weight = new ArrayList<Integer>();
+        
+        int average = 0;
+
+        try {
+            FileInputStream fileInput = new FileInputStream("data.ser");
+            ObjectInputStream inputStream = new ObjectInputStream(fileInput);
+            weight = (ArrayList<Integer>) inputStream.readObject();
+            inputStream.close();   
+            fileInput.close();
+            } catch (IOException e) {
+                e.printStackTrace();  
+            } catch (ClassNotFoundException c){
+                System.out.println("Average Class not found");
+                c.printStackTrace();
+                
+            }
+
+
+        //int[] weight = {77, 80, 79, 90};
+              
+                   
+            
+            Scanner scan = new Scanner(System.in);
+        try {
+            FileOutputStream fileOutput = new FileOutputStream("data.ser");
+            ObjectOutputStream outputStream = new ObjectOutputStream(fileOutput);  
+
+            while(true){   
+            System.out.println("Do you want to enter you weight today?: Yes/No"); 
+            String answer2 = scan.next();
+            if(answer2.equals("No") || answer2.equals("no")){
+                System.out.println("Here are the previous weights: " + weight.toString());
+                scan.close();
+                outputStream.close();
+                fileOutput.close();
+                break;
+            }else{    
+            System.out.println("Please enter your fat fucking weight:");
+            weight.add(scan.nextInt());
+            outputStream.writeObject(weight);
+            outputStream.close();
+            }   
+            System.out.println("Do you want to add more flab? Yes/No");
+            String answer = scan.next();
+
+            if(answer.equals("No") || answer.equals("no")){
+                System.out.println("----------------------------");
+                System.out.println("Ok you fat bitch");
+                System.out.println("Here are the previous weights: " + weight.toString());
+                scan.close();
+                fileOutput.close();
+                break;
+            }else{
+                System.out.println("Ohh fatty wants to add more");
+            }
+            }
+        } catch (IOException e) {
+                e.printStackTrace();
+                return;
+                }
+
+        //R GETS REASSIGNED FOR-EACH ELEMENT IN WEIGHT(data structure)
+        for(int r : weight){ 
+            average += r;
+            //..so R is assigned to each element, example first element in 
+            //weight data structure, then we add average and the first element of weight
+            //which is now in r 77, then r is reassigned again to the second element
+            //so on... for each loop will end when all elements have been assigned
+        }
+        System.out.println("Average weight: "+ average/weight.size());
+        
+
+
+    
+    }
+
+
+
+
+
+
+/*DEAD CODE
+for(i = 0; i<4;i++){
+        average += weight[i]; 
+        //average /= i; 
+
+    }    
+
+//average = average / r; IGNORE WRONG
+
+*/
+}
---
+++ Average/Weight.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * Array List class that stores weight
+ */
+
+
+
+public class Weight extends ArrayList{
+
+    
+
+
+
+
+}
