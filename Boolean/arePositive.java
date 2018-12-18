0692df865e683db5a5d9d698d7c5dfe5e77ac4d6
mAnjra <39240839+mAnjra@users.noreply.github.com>
2018-12-18 21:15:01 +0000

    Update arePositive.java

--- Boolean/arePositive.java
+++ Boolean/arePositive.java
@@ -8,12 +8,13 @@ public class arePositive{
         list.add(-5);
         list.add(3);
 
-        System.out.println("Are all the numbers on the list positive? "+ positive(list));
+    
+        System.out.println("Are all the numbers on the list positive? "+ positivity(list));
 
     }
 
     public static boolean positive(ArrayList<Integer> wist){
-        boolean t = true;
+        boolean t = true;//helper variable inside this method hmmm is there a better a way?*look below
 
         for(int i : wist){
             if(i<0){
@@ -22,4 +23,19 @@ public class arePositive{
         }
         return t;
     }
+
+    /**
+     * By calling the return call within the if statement you can cut the method short if
+     * there is a NEGATIVE number in the list - why go through all of them ? return false simples
+     */
+
+     public static boolean positivity(ArrayList<Integer> eist){
+         for(int f : eist){
+             if(f<0){
+                 return false;
+             }
+         }
+         return true;
+     }
+       
 }
