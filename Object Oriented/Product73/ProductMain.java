0963abcc1e00b2051bb6394bdfcd3b6604878cdd
mAnjra <39240839+mAnjra@users.noreply.github.com>
2018-12-29 20:39:29 +0000

    Update ProductMain.java

--- Object Oriented/Product73/ProductMain.java
+++ Object Oriented/Product73/ProductMain.java
@@ -1,7 +1,6 @@
 public class ProductMain{
     public static void main(String[] args){
         Product bean = new Product("bean", 1.00, 67);
-
-        bean.printProduct();
+            bean.printProduct();
     }
 }
