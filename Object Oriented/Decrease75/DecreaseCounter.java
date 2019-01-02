edfc3d268ec669008a0d31dbab67abd44c288f89
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-01-02 16:31:17 +0000

    Update DecreaseCounter.java

--- Object Oriented/Decrease75/DecreaseCounter.java
+++ Object Oriented/Decrease75/DecreaseCounter.java
@@ -1,22 +1,27 @@
 public class DecreaseCounter{
    private int value;
+   private int initialValue;
 
    public DecreaseCounter(int valueAtStart){
        this.value = valueAtStart;
+       initialValue = this.value;//for the setToInitial method
    }
 
    public void print(){
         System.out.println("Value: "+ this.value);
    }
 
-   public void decreaseValue(){
+   public void decreaseValue(){//75.2
     if(this.value>0){
        this.value -= 1;  
     }  
    }
    
-   public void reset(){
+   public void reset(){//75.3
        this.value = 0;
-   } 
-
+   }  
+   
+   public void setToInitial(){//75.4
+        this.value = initialValue;
+   }  
 }
