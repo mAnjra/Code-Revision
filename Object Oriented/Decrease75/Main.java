30aa0b841f6499ca5d5c62474d05e7a8008b1c47
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-01-02 16:21:43 +0000

    Update Main.java

--- Object Oriented/Decrease75/Main.java
+++ Object Oriented/Decrease75/Main.java
@@ -1,13 +1,20 @@
 public class Main{
     public static void main(String[] args){
         DecreaseCounter counter = new DecreaseCounter(2);
-
+/*75.2
         counter.print();
         counter.decreaseValue();
         counter.print();
         counter.decreaseValue();
         counter.print();
         counter.decreaseValue();
-        counter.print();//no when counter gets to 0 it stops
+        counter.print();//no when counter gets to 0 it stops*/
+        
+        counter.print();
+        counter.reset();
+        counter.print();
+        counter.decreaseValue();
+        counter.print();
+        
     }
 }
