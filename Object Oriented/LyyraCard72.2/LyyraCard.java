a778df6a1cbf1c6ecd0777ab7f2ae52f4232d00a
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-01-09 16:00:59 +0000

    Update LyyraCard.java

--- Object Oriented/LyyraCard72.2/LyyraCard.java
+++ Object Oriented/LyyraCard72.2/LyyraCard.java
@@ -14,9 +14,11 @@ public class LyyraCard{
     }
     
     public void loadMoney(double amount){
-        this.balance += amount;
-        if(this.balance>150){
-            this.balance = 150;
+        if (amount>0) {
+            this.balance += amount;
+            if (this.balance > 150) {
+                this.balance = 150;
+            }
         }
     }
 
