d922e38f186092db8bb32cbdc6d2b0717683d4c8
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-01-24 13:20:32 +0000

    Update BoundedCounter.java

--- Object Oriented/BoundedCounter_/BoundedCounter.java
+++ Object Oriented/BoundedCounter_/BoundedCounter.java
@@ -8,11 +8,20 @@ public class BoundedCounter{
     }
 
     public void next(){
-        if(this.value >= this.upperLimit ){
+        if(this.value >= this.upperLimit ){//this is where the value resets(to 0) if it hits the upperLimit
             this.value = 0;
         }else{
             this.value +=1;
         }   
+    }
+    
+    public void setValue(int value){
+        if(this.value >=0){
+            if(this.upperLimit >= this.value){
+            this.value = value;
+        } 
+
+        }
     }
 
     public int getValue(){
