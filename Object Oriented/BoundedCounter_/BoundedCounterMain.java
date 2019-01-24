55ff4748f8c7736cc5842eed8a735351e18975cb
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-01-24 13:19:05 +0000

    Update BoundedCounterMain.java

--- Object Oriented/BoundedCounter_/BoundedCounterMain.java
+++ Object Oriented/BoundedCounter_/BoundedCounterMain.java
@@ -1,18 +1,38 @@
 public class BoundedCounterMain {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {//in order for the thread to sleep(here its sleeps for a second)
+        //Scanner scan = new Scanner(System.in);
+        BoundedCounter seconds = new BoundedCounter(59);
         BoundedCounter minutes = new BoundedCounter(59);
         BoundedCounter hour = new BoundedCounter(23);
+/*
+        System.out.println("Seconds: ");
+        int s = Integer.parseInt(scan.nextLine());
+        System.out.println("Minutes: ");
+        int m = Integer.parseInt(scan.nextLine());
+        System.out.println("Hours: ");
+        int h = Integer.parseInt(scan.nextLine());
 
+        seconds.setValue(s);
+        minutes.setValue(m);
+        hour.setValue(h);
+
+*/
+        
         int i = 0;
-        while (i < 121) {
-            System.out.println(hour + ":" + minutes);
-            minutes.next();
-            //int t = minutes.getValue();
-            if(minutes.getValue()==0){
+        while (true) {
+            System.out.println(hour + ":" + minutes+":"+seconds);
+           // minutes.next();
+           Thread.sleep(1000);
+            seconds.next();
+            
+            if(seconds.getValue()==0){
+                minutes.next();
+            }
+            if(minutes.getValue()==0&&seconds.getValue()==0){
                 hour.next();
             }
             i++;
+            if(hour.getValue()==1){break;}
         }
     }
 
