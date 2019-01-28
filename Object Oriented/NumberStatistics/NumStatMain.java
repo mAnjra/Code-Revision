bb837c73393672c6b98f99473edf1cf13728f7ff
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-01-28 20:42:21 +0000

    Add files via upload

---
+++ Object Oriented/NumberStatistics/NumStatMain.java
@@ -0,0 +1,13 @@
+public class NumStatMain {
+    public static void main(String[] args){
+        NumberStatistics stats = new NumberStatistics();
+        stats.addNumber(3);
+        stats.addNumber(5);
+        stats.addNumber(1);
+        stats.addNumber(2);
+        System.out.println(" Amount: "+ stats.amountOfNumbersAdded());
+        System.out.println("    Sum: "+ stats.sum());
+        System.out.println("Average: "+ stats.average());
+    
+    }
+}
---
+++ Object Oriented/NumberStatistics/NumberStatistics.java
@@ -0,0 +1,30 @@
+public class NumberStatistics{
+    private int amountOfNumbers;
+    private int sum;
+
+    public NumberStatistics(){
+        this.amountOfNumbers= 0;
+        this.sum= 0;
+    }
+
+    public void addNumber(int number){
+        this.sum += number;
+        this.amountOfNumbers++;
+    }
+
+    public int amountOfNumbersAdded(){
+        return this.amountOfNumbers;
+    }
+
+    public int sum(){
+        return this.sum;
+    }
+
+    public double average(){
+        if( this.amountOfNumbers == 0 ){
+            return 0;
+        }else{
+            return (double) this.sum / this.amountOfNumbers;//have to cast a double. otherwise answer ignores numbers after decimal
+        }
+    }
+}
