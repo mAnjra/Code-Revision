88e40f29033819aaea8e70dc942a561e6b190ab6
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-01-29 22:13:22 +0000

    Update NumStatMain.java

--- Object Oriented/NumberStatistics/NumStatMain.java
+++ Object Oriented/NumberStatistics/NumStatMain.java
@@ -1,6 +1,7 @@
 public class NumStatMain {
     public static void main(String[] args){
         NumberStatistics stats = new NumberStatistics();
+        /*
         stats.addNumber(3);
         stats.addNumber(5);
         stats.addNumber(1);
@@ -8,6 +9,6 @@ public class NumStatMain {
         System.out.println(" Amount: "+ stats.amountOfNumbersAdded());
         System.out.println("    Sum: "+ stats.sum());
         System.out.println("Average: "+ stats.average());
-    
+        */
     }
 }
