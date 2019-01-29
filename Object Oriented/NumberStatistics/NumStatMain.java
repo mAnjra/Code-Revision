541f5f313248e5978324b139edfe897acfecd5c3
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-01-29 22:14:06 +0000

    Update NumStatMain.java

--- Object Oriented/NumberStatistics/NumStatMain.java
+++ Object Oriented/NumberStatistics/NumStatMain.java
@@ -1,6 +1,8 @@
 public class NumStatMain {
     public static void main(String[] args){
         NumberStatistics stats = new NumberStatistics();
+        NumberStatistics even = new NumberStatistics();
+        NumberStatistics odd = new NumberStatistics();
         /*
         stats.addNumber(3);
         stats.addNumber(5);
@@ -10,5 +12,25 @@ public class NumStatMain {
         System.out.println("    Sum: "+ stats.sum());
         System.out.println("Average: "+ stats.average());
         */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type Number:");
+
+        while (true) {
+            int i = Integer.parseInt(scan.nextLine());
+            if (i == -1) {
+                break;
+            }
+            stats.addNumber(i);
+            if(i%2==0){
+                even.addNumber(i);
+            }
+            if(i%2!=0){
+                odd.addNumber(i);
+            }
+        }
+        scan.close();
+        System.out.println("Sum: " + stats.sum());
+        System.out.println("Even: "+ even.sum());
+        System.out.println("Odd: "+ odd.sum());
     }
 }
