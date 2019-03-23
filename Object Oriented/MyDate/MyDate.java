c510049fdf9aec8a4860d4dd1ef83a72fa9f70c9
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-03-23 16:07:39 +0000

    Add files via upload

---
+++ Object Oriented/MyDate/MyDate.java
@@ -0,0 +1,17 @@
+public class MyDate{
+    private int day;
+    private int month;
+    private int year;
+
+    public MyDate(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public toString(){
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+    
+}
