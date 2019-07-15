60adfe638469d7595af478a4956d9f38cd4a37ba
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-07-15 22:25:13 +0100

    Update Changer.java

--- W72CharachterStringChanger/Changer.java
+++ W72CharachterStringChanger/Changer.java
@@ -1,4 +1,18 @@
 public class Changer{
-    public Changer(){}
+    ArrayList<Change> changeList;
 
+    public Changer(){
+        this.changeList = new ArrayList<>();
+    }
+
+    public void addChange(Change change){
+        this.changeList.add(change);
+    }
+
+    public String change(String characterString){
+        for(Change c : changeList){
+            characterString = c.change(characterString);
+        }
+        return characterString;
+    }
 }
