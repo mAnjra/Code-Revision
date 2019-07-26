9bc4cdb0aa3bf655bbe5f97a6ef605ae8ef17556
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-07-26 16:41:17 +0100

    Add files via upload

---
+++ W74ThingsSuitcaseContainer/Things.java
@@ -0,0 +1,21 @@
+public class Things {
+    private String name;
+    private int weight;
+
+    public Things(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public String toString() {
+        return this.name + " (" + this.weight + " kg)";
+    }
+}
