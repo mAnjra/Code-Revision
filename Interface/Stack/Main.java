241491bd6dbf858018c3d16364e1e4b880237fe8
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-09-03 20:07:37 +0100

    Update Main.java

--- Interface/Stack/Main.java
+++ Interface/Stack/Main.java
@@ -23,6 +23,27 @@ public class Main {
         System.out.println(stack2.pop());
         System.out.println(stack2.pop());
 
+        // Polymorphism - using interface variable reference
+        // one variable used - access to many implementations
+        IntStack myStack;
+        DynStack dyanmicStack = new DynStack(5);
+        FixedStack fixedStack = new FixedStack(5);
+
+        // load a dynamic stack
+        myStack = dyanmicStack;
+        for(int i = 0; i < 11 ;i++) myStack.push(i); // pushing using DynStack implementation
+        myStack = fixedStack;
+        for(int i = 0; i < 6; i++) myStack.push(i); // pushing using Fixed Stack implementation
+        
+        // Popping
+        System.out.println("Dynamic Stack: ");
+        myStack = dyanmicStack; // go back to Dynamic stack
+        for(int i = 0; i < 11; i++) System.out.println(myStack.pop());
+
+        System.out.println("Fixed Stack:");
+        myStack = fixedStack;
+        for(int i = 0; i < 6; i++) System.out.println(fixedStack.pop());
+
     }
 
 
