5a130ab1dbb8e53b82caeec5cfee61b40641505f
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-09-30 19:12:46 +0100

    Add files via upload

---
+++ Comparable/SortingCards/Main.java
@@ -0,0 +1,8 @@
+package SortingCards;
+
+public class Main {
+    public static void main(String[] args) {
+        Card first = new Card(2, Card.SPADES);
+        System.out.println(first);
+    }
+}
