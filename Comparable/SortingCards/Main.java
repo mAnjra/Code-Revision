e53cc607526171323b4a2e89e5a8c825bdff376d
mAnjra <39240839+mAnjra@users.noreply.github.com>
2019-10-01 19:34:59 +0100

    Delete Main.java

--- Comparable/SortingCards/Main.java
+++
@@ -1,8 +0,0 @@
-package SortingCards;
-
-public class Main {
-    public static void main(String[] args) {
-        Card first = new Card(2, Card.SPADES);
-        System.out.println(first);
-    }
-}
